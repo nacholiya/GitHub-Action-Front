f053ff6007a051056129bdb22bb35ab20cb7bdb6
nacholiya <nacholiya@gmail.com>
2026-01-09 12:38:11 +0530

    feat: add h3 on home page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Deploy Through CI/CD GitHub Action to GitHub Pages</h2>
+      <h3>I Need to Make an Update so This is the New Page here for You!!</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
